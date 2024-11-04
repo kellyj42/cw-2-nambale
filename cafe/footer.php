55346673a34ff44faedf183b4f936a03e536bf87
kellyj42 <kellyjnambale@gmail.com>
2024-11-04 07:20:37 +0300

    Add files via upload

---
+++ cafe/footer.php
@@ -0,0 +1,47 @@
+<footer>
+    <div class="footer-container">
+        <div class="col-1">
+            <h3>
+                <div class="footer-img">
+                    <img src="img/Untitled (2).png" alt="logo" width="15%">
+                </div>
+            </h3>
+            <div class="col1-p">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Officiis reprehenderit nesciunt expedita ipsa.<br>Deleniti quis corporis ducimus amet cupiditate facilis.</p>
+            </div>
+        </div>
+
+        <div class="col-2">
+            <h3>Links</h3>
+            <div class="para">
+                <p><a href="">home</a></p>
+                <p><a href="">menu</a></p>
+                <p><a href="">contact us</a></p>
+                <p><a href="">about us</a></p>
+            </div>
+        </div>
+
+        <div class="col-1">
+            <h3>Team</h3>
+            <div class="para">
+                <p>swaib</p>
+                <p>kelly</p>
+                <p>doreen</p>
+                <p>dorothy</p>
+            </div>
+        </div>
+
+        <div class="col-1">
+            <h3>Newsletter</h3>
+            <input type="text" name="" id="" placeholder="enter your email">
+            <button type="submit">subscribe</button>
+            <div class="social">
+                <img src="img/facebook-brands-solid (1).svg" alt="">
+                <img src="img/instagram-brands-solid.svg" alt="">
+                <img src="img/skype-brands-solid (1).svg" alt="">
+                <img src="img/twitter-brands-solid.svg" alt="">
+                <img src="img/google-plus-g-brands-solid.svg" alt="">
+            </div>
+        </div>
+    </div>    
+</footer>
---
+++ cafe/script.js
@@ -0,0 +1,5 @@
+
+    document.querySelector('.menu-toggle').addEventListener('click', function() {
+        const menu = document.querySelector('.mega-menu');
+        menu.classList.toggle('active');
+    });

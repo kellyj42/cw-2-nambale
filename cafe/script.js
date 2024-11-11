37734e84b7987ac2201dc091d9f11dc608f5ed4c
kellyj42 <kellyjnambale@gmail.com>
2024-11-11 16:48:21 +0300

    Delete cafe directory

--- cafe/script.js
+++
@@ -1,5 +0,0 @@
-
-    document.querySelector('.menu-toggle').addEventListener('click', function() {
-        const menu = document.querySelector('.mega-menu');
-        menu.classList.toggle('active');
-    });
